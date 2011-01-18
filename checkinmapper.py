29ecead1f6c73359248565b45ce187202aba1467
Joshua Bronson <jabronson@gmail.com>
2011-01-18 14:04:46 -0500

    sketch out gae datastore-backed CheckinStore

--- checkinmapper.py
+++ checkinmapper.py
@@ -1,7 +1,10 @@
 # Copyright 2011 Josh Bronson <jabronson@gmail.com>
 
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
 from functools import wraps
+#from geo.geomodel import GeoModel
+from google.appengine.ext import db
 from itertools import count
 from random import random
 from werkzeug import url_decode
@@ -25,23 +28,50 @@ class MethodRewriteMiddleware(object):
                 environ['REQUEST_METHOD'] = method
         return self.app(environ, start_response)
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = settings.secret_key
+app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
+
+class InvalidId(Exception): pass
+class InvalidInput(Exception): pass
+
 class Checkin(object):
     def __init__(self, lat, lon, val):
-        self.id = None
+        self._id = None
         self.lat = lat
         self.lon = lon
         self.val = val
+        self.time_c = self.time_m = self.time_a = datetime.utcnow()
+
+    @property
+    def id(self):
+        return self._id
 
     def update(self, lat, lon, val):
         self.lat = lat
         self.lon = lon
         self.val = val
+        self.time_m = self.time_a = datetime.utcnow()
 
     def __repr__(self):
         return '<%s id=%r lat=%r lon=%r val=%r>' % (self.__class__.__name__,
             self.id, self.lat, self.lon, self.val)
 
-class InvalidId(Exception): pass
+class GAECheckin(db.Model, Checkin):
+    lat = db.FloatProperty()
+    lon = db.FloatProperty()
+    val = db.FloatProperty()
+    time_c = db.DateTimeProperty()
+    time_m = db.DateTimeProperty()
+    time_a = db.DateTimeProperty()
+
+    def __init__(self, *args, **kw):
+        db.Model.__init__(self, *args, **kw)
+        self.time_c = self.time_m = self.time_a = datetime.utcnow()
+
+    @property
+    def id(self):
+        return self.key().id()
 
 class CheckinStore(object):
     '''
@@ -49,11 +79,11 @@ class CheckinStore(object):
     '''
     def get(self, id):
         raise NotImplementedError
-    def add(self, checkin):
+    def add(self, lat, lon, val):
         raise NotImplementedError
     def remove(self, id):
         raise NotImplementedError
-    def update(self, checkin):
+    def update(self, lat, lon, val):
         raise NotImplementedError
     def __iter__(self):
         raise NotImplementedError
@@ -69,26 +99,73 @@ class TmpCheckinStore(CheckinStore):
 
     def get(self, id):
         try:
-            return self.checkins[int(id)]
+            checkin = self.checkins[id]
         except:
             raise InvalidId(id)
+        checkin.time_a = datetime.utcnow()
+        return checkin
 
-    def add(self, checkin):
+    def add(self, lat, lon, val):
         id = self.counter.next()
-        checkin.id = id
+        checkin = Checkin(lat, lon, val)
         self.checkins[id] = checkin
+        checkin._id = id
+        return checkin
 
     def remove(self, id):
         try:
-            del self.checkins[int(id)]
+            del self.checkins[id]
         except:
             raise InvalidId(id)
 
-    def update(self, checkin):
-        pass # no need to do anything since checkin stored in memory
+    def update(self, id, lat, lon, val):
+        try:
+            checkin = self.checkins[id]
+        except:
+            raise InvalidId(id)
+        checkin.update(lat, lon, val)
 
     def __iter__(self):
         return self.checkins.itervalues()
+
+class GAECheckinStore(CheckinStore):
+    '''
+    CheckinStore which stores data in App Engine's data store.
+    '''
+    def get(self, id):
+        try:
+            key = db.Key.from_path('GAECheckin', id)
+            checkin = db.get(key)
+            assert checkin is not None
+        except: # XXX
+            raise InvalidId
+        checkin.time_a = datetime.utcnow()
+        return checkin
+
+    def add(self, lat, lon, val):
+        checkin = GAECheckin(lat=lat, lon=lon, val=val)
+        id = db.put(checkin).id()
+        return db.get(db.Key.from_path('GAECheckin', id))
+
+    def remove(self, id):
+        try:
+            key = db.Key.from_path('GAECheckin', id)
+            db.delete(key)
+        except: # XXX
+            raise InvalidId
+
+    def update(self, id, lat, lon, val):
+        try:
+            key = db.Key.from_path('GAECheckin', id)
+            checkin = db.get(key)
+            assert checkin is not None
+        except: # XXX
+            raise InvalidId
+        checkin.update(lat, lon, val)
+        checkin.put()
+
+    def __iter__(self):
+        return iter(GAECheckin.all())
 
 def scale(x, bounds):
     '''
@@ -107,15 +184,7 @@ def add_random_checkins(checkinstore, n=10, latbounds=(35, 40),
         lat = scale(random(), latbounds)
         lon = scale(random(), lonbounds)
         val = scale(random(), valbounds)
-        checkin = Checkin(lat, lon, val)
-        checkinstore.add(checkin)
-
-checkinstore = TmpCheckinStore()
-add_random_checkins(checkinstore)
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = settings.secret_key
-app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
+        checkinstore.add(lat, lon, val)
 
 @app.route('/checkins/new')
 def checkins_new_form():
@@ -125,8 +194,6 @@ def checkins_new_form():
     Renders form for new checkin.
     '''
     return render_template('checkins/new.html', settings=settings)
-
-class InvalidInput(Exception): pass
 
 def _checkin_data_from_req():
     try:
@@ -146,8 +213,7 @@ def checkins_new():
         lat, lon, val = _checkin_data_from_req()
     except InvalidInput:
         raise _400_invalid_input
-    checkin = Checkin(lat, lon, val)
-    checkinstore.add(checkin)
+    checkin = checkinstore.add(lat, lon, val)
     flash('checkin saved')
     return redirect(url_for('checkins_edit_form', id=checkin.id))
 
@@ -158,8 +224,9 @@ def checkins_edit_form(id):
     GET /checkins/<id>
     '''
     try:
+        id = int(id)
         checkin = checkinstore.get(id)
-    except InvalidId:
+    except (ValueError, InvalidId):
         raise _404_invalid_id
     return render_template('checkins/edit.html', checkin=checkin,
         settings=settings)
@@ -172,16 +239,16 @@ def checkins_edit(id):
     Updates specified checkin from request data.
     '''
     try:
-        checkin = checkinstore.get(id)
-    except InvalidId:
-        raise _404_invalid_id
-    try:
         lat, lon, val = _checkin_data_from_req()
     except InvalidInput:
         raise _400_invalid_input
-    checkin.update(lat, lon, val)
+    try:
+        id = int(id)
+        checkinstore.update(id, lat, lon, val)
+    except (ValueError, InvalidId):
+        raise _404_invalid_id
     flash('checkin updated')
-    return redirect(url_for('checkins_edit_form', id=checkin.id))
+    return redirect(url_for('checkins_edit_form', id=id))
 
 @app.route('/checkins/<id>', methods=['DELETE'])
 def checkins_delete(id):
@@ -189,8 +256,9 @@ def checkins_delete(id):
     DELETE /checkins/<id>
     '''
     try:
+        id = int(id)
         checkinstore.remove(id)
-    except InvalidId:
+    except (ValueError, InvalidId):
         raise _404_invalid_id
     flash('checkin deleted')
     return redirect(url_for('checkins_view_all'))
@@ -202,6 +270,7 @@ def checkins_view_all():
 
     View all checkins
     '''
+    list(checkinstore)
     return render_template('checkins/view_all.html',
         checkins=checkinstore, settings=settings)
 
@@ -211,7 +280,11 @@ def root():
 
 try:
     from google.appengine.ext.webapp.util import run_wsgi_app
+    app.config['DEBUG'] = settings.debug = False
+    checkinstore = GAECheckinStore()
     run_wsgi_app(app)
 except ImportError:
     app.config['DEBUG'] = settings.debug
+    checkinstore = TmpCheckinStore()
+    add_random_checkins(checkinstore)
     app.run()
